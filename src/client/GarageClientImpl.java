4341dd3124831ecc82c4fbf2627cb039aa76e94f
unknown <jessicarivas92@gmail.com>
2016-11-18 23:35:12 -0700

    adding garageclient impl

---
+++ src/client/GarageClientImpl.java
@@ -0,0 +1,23 @@
+import java.rmi.*;
+import java.rmi.server.*;
+
+/**
+ * This class implements the remote interface 
+ * CallbackClientInterface.
+ * @author M. L. Liu
+ */
+
+public class GarageClientImpl extends UnicastRemoteObject
+     implements GarageClientInterface {
+  
+   public GarageClientImpl() throws RemoteException {
+      super( );
+   }
+
+   public String notifyMe(String message){
+      String returnMessage = "Call back received: " + message;
+      System.out.println(returnMessage);
+      return returnMessage;
+   }      
+
+}// end CallbackClientImpl class   
